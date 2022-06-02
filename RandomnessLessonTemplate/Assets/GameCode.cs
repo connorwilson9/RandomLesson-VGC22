e86de8e59ab6b5c128b9455abd5874f9dbb4b75f
connorwilson9 <connor.wilson@unb.ca>
2022-06-02 09:09:27 -0300

    Upload

---
+++ RandomnessLessonTemplate/Assets/GameCode.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameCode : MonoBehaviour
+{
+    public SpriteRenderer dragon;
+    public SpriteRenderer mouse;
+    public SpriteRenderer pumpkin;
+    public SpriteRenderer lion;
+    public TMP_Text text; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
